c7ef188cf07e490f7dbe657e237182b4f63259ec
Alfarizqi88 <alfarizqi92@gmail.com>
2020-05-04 21:08:57 +0700

    Fix nilai preferensi dan jarak +/-

--- hastop.php
+++ hastop.php
@@ -318,6 +318,126 @@ $jml_kriteria =count($kriteria);
           </div>
         </div>
       </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              Jarak positif (D<sub>i</sub><sup>+</sup>)
+            </div>
+            <div class="panel-body">
+              <table class="table table-striped table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Alternatif</th>
+                    <th>Nama</th>
+                    <th>D<suo>+</sup></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $i=0;
+                  $dplus=array();
+                  foreach($data as $nama=>$krit){
+                    echo "<tr>
+                      <td>".(++$i)."</td>
+                      <th>A{$i}</th>
+                      <td>{$nama}</td>";
+                    foreach($kriteria as $k){
+                      if(!isset($dplus[$i-1])) $dplus[$i-1]=0;
+                      $dplus[$i-1]+=pow($yplus[$k]-$y[$k][$i-1],2);
+                    }
+                    echo "<td>".round(sqrt($dplus[$i-1]),10)."</td>
+                     </tr>\n";
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              Jarak negatif (D<sub>i</sub><sup>-</sup>)
+            </div>
+            <div class="panel-body">
+              <table class="table table-striped table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Alternatif</th>
+                    <th>Nama</th>
+                    <th>D<suo>-</sup></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $i=0;
+                  $dmin=array();
+                  foreach($data as $nama=>$krit){
+                    echo "<tr>
+                      <td>".(++$i)."</td>
+                      <th>A{$i}</th>
+                      <td>{$nama}</td>";
+                    foreach($kriteria as $k){
+                      if(!isset($dmin[$i-1]))$dmin[$i-1]=0;
+                      $dmin[$i-1]+=pow($ymin[$k]-$y[$k][$i-1],2);
+                    }
+                    echo "<td>".round(sqrt($dmin[$i-1]),10)."</td>
+
+                     </tr>\n";
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              Nilai Preferensi(V<sub>i</sub>)
+            </div>
+            <div class="panel-body">
+              <table class="table table-striped table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Alternatif</th>
+                    <th>Nama</th>
+                    <th>V<sub>i</sub></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php 
+                    $i=0;
+                    $V=array();
+                    $Y=array();
+                    $Z=array();                        
+                    $hasilakhir=array();
+                    foreach ($data as $nama => $krit) {
+                    echo "<tr>
+                    <td>".(++$i)."</td>
+                    <th>A{$i}</th>
+                    <td>{$nama}</td>";             
+                    foreach($kriteria as $k){
+                      $V[$i-1]=round(sqrt($dmin[$i-1]),10)/(round(sqrt($dmin[$i-1]),10)+round(sqrt($dplus[$i-1]),10));
+                    }
+                    echo "<td>{$V[$i-1]}</td></tr>\n";
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div> <!--container-->
 
     
